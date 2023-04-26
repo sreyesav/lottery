46eb38aa738bb1528d6d28ebba5dfc7fd19f2952
Roger <rogerdrodulfo@hotmail.com>
2023-04-26 13:25:37 -0500

    Fixed catalog page

--- website/views.py
+++ website/views.py
@@ -28,16 +28,6 @@ def catalog():
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
-
-        if len(note) < 1:
-            flash('Comment is too short', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Comment Added!', category='success')
     return render_template("home.html", user=current_user)
 
 @views.route('/delete-note', methods=['POST'])
